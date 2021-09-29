e1e87753b66d477b989b08950c20854e2fc995da
fruitvodka <varshav0119@gmail.com>
2021-09-29 17:41:07 +0530

    Adding redux-persist and logOut button for IPS space as well

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,19 @@
 import {createStore, combineReducers} from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import localStorage from 'redux-persist/lib/storage'
 import userReducer from './user';
 
+const persistConfig = {
+    key: 'root',
+    storage: localStorage
+  }
+
 const rootReducer = combineReducers({
-    user: userReducer
+    user: persistReducer(persistConfig, userReducer)
 });
 
-export default createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+export const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+export const persistor = persistStore(store)

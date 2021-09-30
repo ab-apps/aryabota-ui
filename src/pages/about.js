5bc08c8a373cea6ea2fbc17677f5384085ce7076
prernarao <prernanewemail@gmail.com>
2021-09-30 13:46:15 +0530

    Open Notion link in new tab

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ function About() {
         <div style={{ width: '100%', height: '100%' }} className="about-page">
             <br />
             <div>We are working on updating this page. <br />
-            Meanwhile, you can take a look at our documentation <a className='hrefs' href='https://aryabota-docs.notion.site/aryabota-docs/AryaBota-316098bf36fc4cef9aeb8ef884a8c2d3'>here</a>
+            Meanwhile, you can take a look at our documentation <a className='hrefs' href='https://aryabota-docs.notion.site/aryabota-docs/AryaBota-316098bf36fc4cef9aeb8ef884a8c2d3' rel="noopener noreferrer" target="_blank">here</a>
             </div>
         </div>
     )

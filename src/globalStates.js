160faa0a95ffef999f16bcfd9e7329615222e29b
prernarao <prernanewemail@gmail.com>
2021-12-01 14:33:13 +0530

    Updating Discord link

--- src/globalStates.js
+++ src/globalStates.js
@@ -6,7 +6,7 @@ import React from 'react';
  */
 export const MazeState = React.createContext([{}, () => {}]);
 export const Constants = {
-    informationMessage: 'Welcome! We hope you enjoyed learning with AryaBota. To join our discord server, go to https://discord.gg/nEHKaXBn!',
+    informationMessage: 'Welcome! We hope you enjoyed learning with AryaBota. To join our discord server, go to https://discord.com/invite/9eyHmGn7m2',
     clientId: '214753177186-nt5lqnh5nggo5l11ur0qeb3onqibmst7.apps.googleusercontent.com',
     disclaimer: 'The data collected as a part of this project will be anonymised and used for research purposes. No personal information will be disclosed.'
 };
